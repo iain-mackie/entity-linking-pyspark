ca050f04a6a79f6a0dd7a46f89780cc047eb1566
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-03-16 16:03:38 +0000

    starting to parse skeleton

--- parse_trec_car.py
+++ parse_trec_car.py
@@ -50,8 +50,9 @@ def parse_bodies(b):
 def parse_paragraph(skeleton_subclass, spacy_nlp):
     print('paragraph.get_text()')
     print(type(skeleton_subclass.paragraph.get_text()))
-    raw_text = str(skeleton_subclass.paragraph.get_text().encode("utf-8").decode("utf-8"))
+    raw_text = str(skeleton_subclass.paragraph.get_text().encode("utf-8"))
     print(raw_text)
+    print(type(raw_text))
     doc = spacy_nlp(text=raw_text)
     print(list(doc.ents))
     return [skeleton_subclass.paragraph.para_id,
