4cab2c6d47686777fd748b644247387027394fd7
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-03-18 18:12:47 +0000

    testing building full DataFrame

--- job.py
+++ job.py
@@ -16,6 +16,7 @@ def run_job(read_path, write_path, num_pages=1, print_intervals=100, write_outpu
     spark = SparkSession.builder.appName('trec_car').getOrCreate()
     spacy_nlp = spacy.load("en_core_web_sm")
     t_start = time.time()
+    t_union = 0
     with open(read_path, 'rb') as f:
         for i, page in enumerate(iter_pages(f)):
 
@@ -28,14 +29,17 @@ def run_job(read_path, write_path, num_pages=1, print_intervals=100, write_outpu
                 df = parse_page(page=page, i=i, spark=spark, spacy_nlp=spacy_nlp)
             else:
                 new_row = parse_page(page=page, i=i, spark=spark, spacy_nlp=spacy_nlp)
+                t_union_start = time.time()
                 df = df.union(new_row)
+                t_union += time.time() - t_union_start
 
             if (i % print_intervals == 0):
                 # prints update at 'print_pages' intervals
                 print('----- page {} -----'.format(i))
                 print(page.page_id)
                 time_delta = time.time() - t_start
-                print('time elapse: {} --> time / page: {}'.format(time_delta, time_delta/(i+1)))
+                print('time elapse: {} --> time / page: {} (time / union: {})'.format(
+                    time_delta, time_delta/(i+1), t_union/(i+1)))
 
     time_delta = time.time() - t_start
     print('PROCESSED DATA: {} --> processing time / page: {}'.format(time_delta, time_delta/(i+1)))
@@ -65,8 +69,8 @@ if __name__ == '__main__':
     read_path = '/nfs/trec_car/data/pages/unprocessedAllButBenchmark.Y2.cbor'
     #read_path = '/nfs/trec_car/entity_processing/trec-car-entity-processing/data/test.pages.cbor'
     write_path = '/nfs/trec_car/data/test_entity/full.parquet'
-    num_pages = 10000
-    print_intervals = 100
+    num_pages = 100000
+    print_intervals = 1000
     write_output = True
     run_job(read_path=read_path, write_path=write_path, num_pages=num_pages, print_intervals=print_intervals,
             write_output=write_output)
