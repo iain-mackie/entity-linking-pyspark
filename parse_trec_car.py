416b51b0e669de388139b847fc4569fb1aba3553
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-03-18 16:29:50 +0000

    testing building full DataFrame

--- parse_trec_car.py
+++ parse_trec_car.py
@@ -21,10 +21,10 @@ page_schema = StructType([
     StructField("categoryIds", ArrayType(StringType(), True), True),
     StructField("inlinkIds", ArrayType(StringType(), True), True),
     StructField("inlinkAnchors", ArrayType(
-        ArrayType(
+        StructType([
             StructField("user", StringType()),
             StructField("product", IntegerType())
-        ), True), True)
+        ]), True), True)
 ])
 page_names = ["idx", "page_id", "page_name", "page_type", "page_meta", "skeleton"]
 
