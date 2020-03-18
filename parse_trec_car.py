c4f7ba883f03c28062d908ea7a246cb0470aea3d
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-03-18 17:23:12 +0000

    testing building full DataFrame

--- parse_trec_car.py
+++ parse_trec_car.py
@@ -163,7 +163,7 @@ def parse_page_1(page, i, spark, spacy_nlp, page_schema=page_schema_1, write_par
                  page.page_meta.categoryNames,
                  page.page_meta.categoryIds,
                  page.page_meta.inlinkIds,
-                 page.page_meta.inlinkAnchors,
+                 # page.page_meta.inlinkAnchors,
                  # parse_skeleton(skeleton=page.skeleton, spacy_nlp=spacy_nlp, write_para=True),
                 )
             ], schema=page_schema)
