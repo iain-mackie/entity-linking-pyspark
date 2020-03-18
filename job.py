d99a6caa32a939d9f622875961c9aa69d9300163
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-03-18 12:54:07 +0000

    ready to test on smaller cbor file

--- job.py
+++ job.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     read_path = '/nfs/trec_car/entity_processing/trec-car-entity-processing/data/test.pages.cbor'
     write_path = '/nfs/trec_car/data/test_entity/test.json'
     num_pages = 10000
-    print_intervals = 50
+    print_intervals = 10
     write_output = True
     run_job(read_path=read_path, write_path=write_path, num_pages=num_pages, print_intervals=print_intervals,
             write_output=write_output)
