641ee6d0c9911e50e8c7db085b8ed88d6edf49bc
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-03-20 14:55:32 +0000

    stopped writing

--- pyspark_job.py
+++ pyspark_job.py
@@ -122,7 +122,7 @@ def spark_processing(pages_as_pickles):
     df = df.withColumn("page_id", page_id_udf("page_pickle"))
     df = df.withColumn("page_name", page_name_udf("page_pickle"))
     df = df.withColumn("page_type", page_type_udf("page_pickle"))
-    # df = df.withColumn("redirect_names", page_redirect_names_udf("page_pickle"))
+    df = df.withColumn("redirect_names", page_redirect_names_udf("page_pickle"))
     # df = df.withColumn("disambiguation_names", page_disambiguation_names_udf("page_pickle"))
     # df = df.withColumn("disambiguation_ids", page_disambiguation_ids_udf("page_pickle"))
     # df = df.withColumn("category_names", page_category_names_udf("page_pickle"))
