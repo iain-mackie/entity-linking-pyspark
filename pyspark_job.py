33f457a0dc85491932a28f6ebd0c05d49ee45a9b
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-03-24 11:58:09 +0000

    added writing to file

--- pyspark_job.py
+++ pyspark_job.py
@@ -208,7 +208,7 @@ def pyspark_processing(dir_path):
         synthetic_skeleton, synthetic_paragraphs = parse_skeleton(skeleton=skeleton, spacy_model=spacy_model)
 
 
-        return (bytearray(pickle.dumps(synthetic_skeleton)), bytearray(pickle.dumps(synthetic_paragraphs)))
+        return bytearray(pickle.dumps(synthetic_skeleton)), bytearray(pickle.dumps(synthetic_paragraphs))
 
 
     # TODO -  sythetics_inlink_anchors
